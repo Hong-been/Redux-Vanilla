1e3345f756ff9040821c866672ce2df1e34fd827
HongBeen <redbean-2@naver.com>
2022-03-09 06:01:33 +0900

    todo app with redux

--- src/index.js
+++ src/index.js
@@ -7,15 +7,19 @@ const ul = document.querySelector("ul");
 const ADD = "ADD";
 const DELETE = "DELETE";
 
-const reducer = (state, action)=>{
-  if(!state) return { };
+const dispatchAddTodo = (payload)=>{
+  store.dispatch({type:ADD,payload});
+}
+const dispatchDeleteTodo = (payload)=>{
+  store.dispatch({type:DELETE,payload});
+}
 
-  const cur = state.count;
+const reducer = (state=[], action)=>{
   switch(action.type){
     case ADD:
-      return [];
+      return [...state, action.payload];
     case DELETE:
-      return [];
+      return state.filter(({id})=> id!==action.payload.id);
     default:
       return state;
   }
@@ -23,18 +27,33 @@ const reducer = (state, action)=>{
 
 const store =  createStore(reducer);
 
-const addTodo = (id,todo)=>{
-  const li = document.createElement("li");
-  li.innerText = todo;
-  ul.appendChild(li);
+const deleteTodo = (id)=>{
+  dispatchDeleteTodo({id});
 }
+
+const paintTodos = ()=>{
+  ul.innerHTML='';
+  store.getState().forEach(({id,todo})=>{
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+
+    btn.innerText="DEL";
+    btn.addEventListener("click",()=>deleteTodo(id));
+    li.id=id;
+    li.textContent=todo;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  })
+}
+
+store.subscribe(paintTodos);
 
 const handleSubmit = (e)=>{
   e.preventDefault();
   const id = Date.now();
   const todo = input.value;
   input.value = '';
-  addTodo(id,todo);
+  dispatchAddTodo({id,todo});
 }
 
 form.addEventListener('submit',handleSubmit);
