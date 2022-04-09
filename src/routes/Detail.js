5a4735d3b42eb968670ba78efcc01c2d6e296862
HongBeen <redbean-2@naver.com>
2022-04-10 06:03:57 +0900

    fix

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -4,13 +4,14 @@ import { useParams } from 'react-router-dom';
 
 const Detail = () => {
   const todos = useSelector(store => store);
-  const id = useParams().id;
+  const id = parseInt(useParams().id);
   return (
     <>
     <h1>Detail</h1>
-    <div>{todos.filter(todo => todo.id==id).map(todo=>todo.text)}</div>
+    <div>{todos.filter(todo => todo.id===id).map(todo=>todo.text)}</div>
     </>
   )
 }
 
 export default Detail;
+//https://nomadcoders.co/redux-for-beginners/lectures/1623
